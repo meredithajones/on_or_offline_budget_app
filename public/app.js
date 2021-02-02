ac298072b8d35552d4e1d0fea4cb82e9d0399c3f
Meredith Jones <mj19003@gmail.com>
2021-02-02 17:34:03 -0500

    Cleaned up comments

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 // Registering our service worker (Boilerplate code copied from Google web fundamentals Service Worker Registration)
 // if statement checks to see if the current browser supports service workers
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register("service-worker.js")
-      .then ((reg) => console.log("service worker registered", reg))
-      .catch((err) => console.log ("service worker not registered", err));
-    });
-  }
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//       navigator.serviceWorker.register("service-worker.js")
+//       .then ((reg) => console.log("service worker registered", reg))
+//       .catch((err) => console.log ("service worker not registered", err));
+//     });
+//   }
   
--- public/db.js
+++ public/db.js
@@ -1,14 +1,3 @@
-//Offline Data
-// db.enablePersistence()
-//     .catch(err => {
-//         if (err.code == 'failed-precondition'){
-//             //probably multiple tabs open
-//             console.log("persistence failed");
-//         }else if (err.code == "unimplemented"){
-//             //lack of browser support
-//             console.log("persistence is not available");
-//         }
-//     })
 
 let db;
 // create a new db request for a "budget_app" database.
